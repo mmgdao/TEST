f2425a0de508f2591a7fc3bca26669382b6a3868
mmgdao <145902064+mmgdao@users.noreply.github.com>
2023-10-17 18:58:01 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-const num = document.getElementsByClassName("button");
+let num = document.getElementsByClassName("button");
+let result = document.getElementById("result");
+let kigou = ["+","-","*","/","."];
+let number = ["1","2","3","4","5","6","7","8","9"]
 
 function clickbutton(num) {
-    console.log(num);
+    if (num.innerHTML == "AC") {
+        result.innerHTML = 0;
+    }else if(kigou.includes(num.innerHTML)) {
+        if((result.innerHTML == 0) || (kigou.includes(result.innerHTML.slice(-1)) )) {
+            return;
+        }else  {
+            result.innerHTML += num.innerHTML;
+        }     
+    }else if((num.innerHTML == "00") || (num.innerHTML == "0")) {
+        if(number.includes(result.innerHTML.slice(-1)))
+            result.innerHTML += num.innerHTML;
+        else {
+            return;
+        }
+
+    }else if(result.innerHTML == 0) {
+        result.innerHTML = num.innerHTML;
+    }else {
+        result.innerHTML += num.innerHTML;
+    }
+    console.log(result.innerHTML);
+    
+}
+
+function equalbutton() {
+    console.log(eval(result.innerHTML));
+    result.innerHTML = eval(result.innerHTML);
 }
